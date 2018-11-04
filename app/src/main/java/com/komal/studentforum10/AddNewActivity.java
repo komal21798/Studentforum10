c7e5599f368515d2bf131eb07bf509ec27dbf645
Komal Modi <komal21798@gmail.com>
2018-11-04 19:53:00 +0530

    Added setup activity(adding profile image and user name)

--- app/src/main/java/com/komal/studentforum10/AddNewActivity.java
+++ app/src/main/java/com/komal/studentforum10/AddNewActivity.java
@@ -3,24 +3,30 @@ package com.komal.studentforum10;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddNewActivity extends AppCompatActivity implements NewPostFragment.OnFragmentInteractionListener, NewThreadFragment.OnFragmentInteractionListener {
-
-
-    public void onSubmitClick(View view){
-
-        Intent myIntent = new Intent(AddNewActivity.this, ThreadSubmittedActivity.class);
-        myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(myIntent);
-        finish();
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +57,7 @@ public class AddNewActivity extends AppCompatActivity implements NewPostFragment
             }
 
         });
+
     }
 
     @Override
