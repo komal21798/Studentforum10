0b5119ba5863a28f866b2a37c17279df16677464
Komal Modi <komal21798@gmail.com>
2018-12-10 16:14:41 +0530

    added deleting posts functionality

--- app/src/main/java/com/komal/studentforum10/ExploreFeedRecyclerAdapter.java
+++ app/src/main/java/com/komal/studentforum10/ExploreFeedRecyclerAdapter.java
@@ -1,16 +1,21 @@
 package com.komal.studentforum10;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.text.format.DateFormat;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -222,6 +227,54 @@ public class ExploreFeedRecyclerAdapter extends RecyclerView.Adapter<ExploreFeed
             holder.deleteReportPost.setVisibility(View.INVISIBLE);
         }
 
+        //for deleteing posts
+        holder.deleteReportPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(context, v);
+                MenuInflater inflater = popup.getMenuInflater();
+                inflater.inflate(R.menu.popupactions, popup.getMenu());
+                popup.show();
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+
+                        boolean choice;
+
+                        if (item.getTitle().equals("Delete")) {
+
+                            final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                            builder.setMessage(" Are you sure you want to delete the post?");
+                            builder.setCancelable(true);
+                            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                }
+                            });
+                            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                    firebaseFirestore.collection("Posts").document(exploreFeedId).delete();
+                                    firebaseFirestore.collection("Threads/" + postThread + "/Posts/").document(exploreFeedId).delete();
+                                    removeAt(holder.getAdapterPosition());
+                                    notifyDataSetChanged();
+                                }
+                            });
+
+                            AlertDialog alertDialog = builder.create();
+                            alertDialog.show();
+
+
+                        }
+                        return true;
+                    }
+                });
+            }
+        });
+
 
     }
 
@@ -296,6 +349,13 @@ public class ExploreFeedRecyclerAdapter extends RecyclerView.Adapter<ExploreFeed
             postCommentCount.setText(count + " "); //Space so no error while converting to string
         }
 
+    }
+
+    //removing deleted posts from recycler view
+    public void removeAt(int position) {
+        exploreFeedList.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, exploreFeedList.size());
     }
 }
 
--- app/src/main/java/com/komal/studentforum10/HomeFeedRecyclerAdapter.java
+++ app/src/main/java/com/komal/studentforum10/HomeFeedRecyclerAdapter.java
@@ -1,16 +1,21 @@
 package com.komal.studentforum10;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.text.format.DateFormat;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -216,20 +221,65 @@ public class HomeFeedRecyclerAdapter extends RecyclerView.Adapter<HomeFeedRecycl
             @Override
             public void onClick(View v) {
 
-                    Intent commentsIntent = new Intent(context, CommentsActivity.class);
-                    commentsIntent.putExtra("threadPageId", homeFeedId);
-                    context.startActivity(commentsIntent);
-
-
+                Intent commentsIntent = new Intent(context, CommentsActivity.class);
+                commentsIntent.putExtra("threadPageId", homeFeedId);
+                context.startActivity(commentsIntent);
 
             }
         });
 
         //for showing delete/report popup menu
-        if(!currentUserId.equals("M4S0hiNILmTuj1nEKp3NCGvfiiF2"))
-        {
+        if (!currentUserId.equals("M4S0hiNILmTuj1nEKp3NCGvfiiF2")) {
             holder.deleteReportPost.setVisibility(View.INVISIBLE);
         }
+
+        //deleting posts
+        holder.deleteReportPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(context, v);
+                MenuInflater inflater = popup.getMenuInflater();
+                inflater.inflate(R.menu.popupactions, popup.getMenu());
+                popup.show();
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+
+                        boolean choice;
+
+                        if (item.getTitle().equals("Delete")) {
+
+                            final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                            builder.setMessage(" Are you sure you want to delete the post?");
+                            builder.setCancelable(true);
+                            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                }
+                            });
+                            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                    firebaseFirestore.collection("Posts").document(homeFeedId).delete();
+                                    firebaseFirestore.collection("Threads/" + postThread + "/Posts/").document(homeFeedId).delete();
+                                    removeAt(holder.getAdapterPosition());
+                                    notifyDataSetChanged();
+                                }
+                            });
+
+                            AlertDialog alertDialog = builder.create();
+                            alertDialog.show();
+
+
+                        }
+                        return true;
+                    }
+                });
+            }
+        });
 
 
     }
@@ -320,5 +370,12 @@ public class HomeFeedRecyclerAdapter extends RecyclerView.Adapter<HomeFeedRecycl
             postCommentCount.setText(count + " "); //Space so no error while converting to string
         }
 
+    }
+
+    //removing deleted posts from recycler view
+    public void removeAt(int position) {
+        homeFeedList.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, homeFeedList.size());
     }
 }
